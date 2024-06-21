9c83fb5e72a6fa7e6d0f5f7781b0f43e545f08a9
Leticia Jyo <leticia.jyo@gmail.com>
2024-06-21 15:13:55 +0200

    feat: create cetegories nest module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule],
   controllers: [],
   providers: [],
 })
