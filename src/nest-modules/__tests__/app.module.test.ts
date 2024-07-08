1d29a9e38c21e15fe8a02f0c37003ff93985ed91
Leticia Jyo <leticia.jyo@gmail.com>
2024-07-08 13:45:04 +0200

    refactor: reorganize nest modules folder

--- src/nest-modules/__tests__/app.module.test.ts
+++ src/nest-modules/__tests__/app.module.test.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppModule } from 'src/app.module';
+import { AppModule } from 'src/nest-modules/app.module';
 
 describe('App Module', () => {
   it('should inject dependencies correctly', async () => {
