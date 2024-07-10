34969010a8cf3d422fb3f6014166ef4818e62536
Leticia Jyo <leticia.jyo@gmail.com>
2024-07-10 17:08:45 +0200

    feat: add not found and domain error handling

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './nest-modules/app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ResponseDataWrapperInterceptor } from './nest-modules/shared/interceptors/response-data-wrapper.interceptor';
+import { NotFoundErrorFilter } from './nest-modules/shared/filters/not-found-error.filter';
+import { EntityValidationErrorFilter } from './nest-modules/shared/filters/entity-validation-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,11 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalInterceptors(new ResponseDataWrapperInterceptor());
+
+  app.useGlobalFilters(
+    new NotFoundErrorFilter(),
+    new EntityValidationErrorFilter(),
+  );
 
   await app.listen(3000);
 }
---
+++ src/nest-modules/shared/filters/entity-validation-error.filter.ts
@@ -0,0 +1,25 @@
+import { EntityValidationError } from '@core/shared/domain/validators/validation.error';
+import { Catch, ExceptionFilter, ArgumentsHost } from '@nestjs/common';
+import { Response } from 'express';
+import { union } from 'lodash';
+
+@Catch(EntityValidationError)
+export class EntityValidationErrorFilter implements ExceptionFilter {
+  catch(exception: EntityValidationError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    response.status(422).json({
+      statusCode: 422,
+      error: 'Unprocessable Entity',
+      message: union(
+        ...exception.errors.reduce(
+          (acc, error) =>
+            acc.concat(
+              typeof error === 'string' ? [[error]] : Object.values(error),
+            ),
+          [],
+        ),
+      ),
+    });
+  }
+}
