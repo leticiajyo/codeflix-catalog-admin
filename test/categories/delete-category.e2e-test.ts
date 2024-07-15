71c8990ac617bfb891eb67faa38ceff43b8dd35f
Leticia Jyo <leticia.jyo@gmail.com>
2024-07-15 16:45:04 +0200

    test: add e2e tests for delete category

---
+++ test/categories/delete-category.e2e-test.ts
@@ -0,0 +1,57 @@
+import request from 'supertest';
+
+import { Category } from '@core/category/domain/category.entity';
+import { ICategoryRepository } from '@core/category/domain/category.repository';
+import { startApp } from 'test/e2e.helper';
+import { CATEGORY_PROVIDERS } from 'src/nest-modules/categories/categories.providers';
+
+describe('E2E Categories Controller', () => {
+  describe('/delete/:id (DELETE)', () => {
+    const appHelper = startApp();
+
+    describe('should a response error when id is invalid or not found', () => {
+      const arrange = [
+        {
+          id: '88ff2587-ce5a-4769-a8c6-1d63d29c5f7a',
+          expected: {
+            message:
+              'Category Not Found using ID 88ff2587-ce5a-4769-a8c6-1d63d29c5f7a',
+            statusCode: 404,
+            error: 'Not Found',
+          },
+        },
+        {
+          id: 'fake id',
+          expected: {
+            statusCode: 400,
+            message: 'Validation failed (uuid is expected)',
+            error: 'Bad Request',
+          },
+        },
+      ];
+
+      test.each(arrange)('when id is $id', async ({ id, expected }) => {
+        return request(appHelper.app.getHttpServer())
+          .delete(`/categories/${id}`)
+          .expect(expected.statusCode)
+          .expect(expected);
+      });
+    });
+
+    it('should delete a category response with status 204', async () => {
+      const categoryRepo = appHelper.app.get<ICategoryRepository>(
+        CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide,
+      );
+      const category = Category.fake().oneCategory().build();
+      await categoryRepo.insert(category);
+
+      await request(appHelper.app.getHttpServer())
+        .delete(`/categories/${category.categoryId.id}`)
+        .expect(204);
+
+      await expect(
+        categoryRepo.findById(category.categoryId),
+      ).resolves.toBeNull();
+    });
+  });
+});
